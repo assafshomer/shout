67b202339d28ed318b24e0917798c4d05927a107
Assaf Shomer <assafshomer@gmail.com>
2013-05-26 12:21:41 +0300

    Create Posts controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Shout::Application.routes.draw do
+  get "posts/new"
+  get "posts/create"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
