6d2750752051ff3b14c840e667ddcc52bfb4241e
Assaf Shomer <assafshomer@gmail.com>
2013-05-26 14:37:49 +0300

    Create Post model, migrate, text_are for the post on the new page

---
+++ spec/models/post_spec.rb
@@ -0,0 +1,5 @@
+require 'spec_helper'
+
+describe Post do
+ it { should respond_to(:content) }
+end
--- spec/requests/post_pages_spec.rb
+++ spec/requests/post_pages_spec.rb
@@ -3,5 +3,5 @@ require 'spec_helper'
 describe "PostPages" do
 	subject { page }
 	before { visit root_path }
-	it { should have_selector('textarea#shout_content') }
+	it { should have_selector('textarea') }
 end
