a6dd09556808b3b72968113617e821b28298440c
Assaf Shomer <assafshomer@gmail.com>
2013-05-27 13:15:46 +0300

    Refactor posts form into partial and button name into views helper

---
+++ app/helpers/views_helper.rb
@@ -0,0 +1,5 @@
+module ViewsHelper
+	def submit_button_title
+		'speak up'
+	end
+end
--- spec/requests/post_pages_spec.rb
+++ spec/requests/post_pages_spec.rb
@@ -1,28 +1,29 @@
 require 'spec_helper'
-
+include ViewsHelper
 describe "PostPages" do
 	subject { page }
+	let!(:button) { submit_button_title }
 	before { visit root_path }
 	it { should have_selector('textarea#post_content') }
 	it { should have_xpath("//textarea[@placeholder='Think inside the box...']") }
 	it { should have_selector('input#shoutup_button') }
-	it { should have_xpath("//input[@value='ShoutUp']") }
+	it { should have_xpath("//input[@value=\'#{button}\']") }
 	describe "validations" do
 		describe "clicking the post button with an empty post should raise an error" do
-			before { click_button 'ShoutUp' }
+			before { click_button button }
 			it { should have_selector('div.alert.alert-error', text: '2 errors') }
 		end
 		describe "clicking the post button with a single character post should raise an error" do
 			before do
 			  fill_in 'post_content', with: 'x'
-			  click_button 'ShoutUp'
+			  click_button button
 			end
 			it { should have_selector('div.alert.alert-error', text: '1 error') }
 		end
 		describe "posting OK should not raise any errors" do
 			before do
 			  fill_in 'post_content', with: 'OK'
-			  click_button 'ShoutUp'
+			  click_button button
 			end
 			it { should_not have_selector('div.alert.alert-error', text: 'error') }
 		end		
@@ -30,7 +31,7 @@ describe "PostPages" do
 	describe "persistance" do
 		before { fill_in 'post_content', with: 'foobar' }
 		it "should save the post to the db" do
-			expect {click_button 'ShoutUp'}.to change(Post, :count).by(1)
+			expect {click_button button}.to change(Post, :count).by(1)
 		end
 	end
 end
