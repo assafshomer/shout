eb5e412a64249c44ab28258b8445b8c5478b331d
Assaf Shomer <assafshomer@gmail.com>
2013-06-17 15:22:34 +0300

    Posts can accomodate new lines now

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,20 +1,34 @@
 module PostsHelper
 
-  def wrap(content)
-    sanitize(raw(content.split.map{ |s| wrap_long_string(s) }.join(' ')))
+  def new_wrap(content)    
+    array=replace_newline_with_br(content).split
+    new_array=array.map do |string|
+      smart_wrap(string)
+    end
+    sanitize(new_array.join(' '))
   end
 
-  def find_new_lines(string)
-    string.split("\n")
+  def replace_newline_with_br(string)
+    result=string.strip.gsub("\r\n","<br/>").gsub("\n", "<br/>").gsub("\r","<br/>")
   end
+
+  def wrap_long_string(text, max_width = 30)
+    zero_width_space = "&#8203;"
+    regex = /.{1,#{max_width}}/
+    (text.length < max_width) ? text : 
+                                text.scan(regex).join(zero_width_space)
+  end
+
+  def smart_wrap(text, max_width = 30)
+    array=text.split("<br/>")
+    new_array =  array.map do |string|
+      wrap_long_string(string)
+    end
+    new_array.join("<br/>")
+  end  
 
   private
 
-    def wrap_long_string(text, max_width = 30)
-      zero_width_space = "&#8203;"
-      regex = /.{1,#{max_width}}/
-      (text.length < max_width) ? text : 
-                                  text.scan(regex).join(zero_width_space)
-    end
+
 
 end
--- spec/helpers/posts_helper_spec.rb
+++ spec/helpers/posts_helper_spec.rb
@@ -1,13 +1,34 @@
 require 'spec_helper'
 
+space="a
+
+b"
+
 describe PostsHelper do	
-	describe "wrap" do
+	describe "new_wrap" do
 		describe "should remove all new lines" do
-			specify{wrap('string string').should == 'string string'}
-			specify{wrap('a'*31).should_not == 'a'*31}
+			specify{new_wrap('string string').should == 'string string'}
+			specify{new_wrap('a'*31).should_not == 'a'*31}						
+			specify{new_wrap('a'*27+"<br/>"+'v').should == 'a'*27+"<br />"+'v' }
+			specify{new_wrap('a'*31+"<br/>"+'v').should == 'a'*30+"&#8203;"+'a'+"<br />"+'v' }
+			specify{new_wrap('assaf <br /> shomer').should == 'assaf <br /> shomer'}
+			specify{new_wrap("a\r\nb").should == 'a<br />b'}
+			specify{new_wrap("assaf\nshomer\n\nis").should == 'assaf<br />shomer<br /><br />is'}
+			specify{new_wrap(space).should == 'a<br /><br />b'}
 		end	
 	end
 
+	describe "replace_newline_with_br" do
+		specify{replace_newline_with_br("a\nb\nc" ).should == "a<br/>b<br/>c" }
+		specify{replace_newline_with_br("a\n\nb\nc" ).should == "a<br/><br/>b<br/>c" }
+		specify{replace_newline_with_br("a\n\n\n" ).should == "a" }
+		specify{replace_newline_with_br("\n\n\na" ).should == "a" }
+		specify{replace_newline_with_br("\n\n\na\nb\n\n\n" ).should == "a<br/>b" }
+	end
 
+	describe "smart wrap" do
+		specify{smart_wrap('a'*31+"<br/>"+'bc').should == 'a'*30+"&#8203;"+'a'+"<br/>"+'bc'}
+		specify{smart_wrap('a'*29+"<br/>"+'bc').should == 'a'*29+"<br/>"+'bc'}		
+	end
 
 end
