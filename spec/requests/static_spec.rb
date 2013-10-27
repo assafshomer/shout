13245caadc0c1affcfbdf6996ec8cb099a5cd466
Assaf Shomer <assafshomer@gmail.com>
2013-10-27 12:40:11 +0200

    Remove duplicate shared example

--- spec/requests/static_spec.rb
+++ spec/requests/static_spec.rb
@@ -3,12 +3,6 @@ include TestHelper
 
 describe "Static" do
 	subject { page }
-  shared_examples_for "all pages" do 
-		it { should have_title app_title+" - " + page_title }
-		it { should have_content('footer') }	
-		it { should have_link home_title }
-		it { should have_link tile_title }			
-	end
 
   describe "Home page" do
   	let!(:page_title) { "Home" }
